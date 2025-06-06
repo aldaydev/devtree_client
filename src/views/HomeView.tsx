64a18cdfa77bdbab1dfd933e6cff2fe52a17a1ba
aldaydev <rafaldayparejo@gmail.com>
2025-06-06 03:58:26 +0200

    working on home view

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -6,12 +6,20 @@ export default function HomeView() {
             <main className="bg-gray-100 lg:py-10 pt-4 min-h-[calc(100dvh-85px)] bg-no-repeat lg:bg-home lg:bg-home-xl bg-right-top px-[10%] res-padding">
 
                 <div className="flex items-center">
-                    <div className="basis-[50%] flex flex-col gap-10">   
-                        <h1 className="text-4xl text-center text-balance font-black lg:text-5xl">
+                    <div className="basis-[50%] flex flex-col gap-4">
+
+                        <h1 className="text-4xl text-center text-balance font-black lg:text-4xl">
                             Todas tus <span className="text-orange">Redes Sociales </span> de artista en un enlace
                         </h1>
 
-                        <p className="text-slate-800 text-xl">Únete a la comunidad de developers compatiendo tus perfiles de GitHub, LinkedIn, Youtube, Instagram, Facebook y más</p>
+                        <p className="text-slate-800 text-xl mb-5">Spotify, Youtube, Amazon Music, Instagram y todos los enlaces que necesitas para compartir tu proyecto musical con el mundo</p>
+
+                        <p className="text-slate-800 text-xl font-bold">¿Cómo funciona?</p>
+
+                        <p className="text-slate-800 text-xl">1. Comprueba que tu nickname está disponible</p>
+                        <p className="text-slate-800 text-xl">2. Crea tu cuenta en menos de 1 minuto</p>
+                        <p className="text-slate-800 text-xl">3. Añade tus enlaces</p>
+                        <p className="text-slate-800 text-xl">4. Comparte tu arte con el mundo</p>
                     </div>
                     <div className="basis-[50%] flex justify-end">
                         <img src="/mlink_illustration.svg" className="w-full h-auto object-cover max-w-[300px] lg:max-w-[500px] "/>
@@ -20,8 +28,8 @@ export default function HomeView() {
 
                 
                 <div className="mx-[10%] md:mx-[15%] mt-[50px]">
-                    <h3 className="text-dark text-3xl font-black text-center mb-6">
-                        ¿ESTÁ MI NOMBRE ARTÍSTICO DISPONIBLE?
+                    <h3 className="text-dark text-2xl font-black text-center mb-6">
+                        COMPRUEBA SI TU NOMBRE ARTÍSTICO ESTÁ DISPONIBLE
                     </h3>
                     <SearchForm/>
                 </div>
